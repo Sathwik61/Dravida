7bf343fd257f58c869bb01074bf1458a2ce0a276
Sathwik <sathwikacharya61@gmail.com>
2023-07-22 19:04:59 +0530

    Added nav bar first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    fontFamily: {
+      Kanit: [sans - serif],
+      DMSans: [sans - serif],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
